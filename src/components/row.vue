16f5605b5eb7bb9e9b5c3d03badcf6c625fdabd9
sillerq <398568296@qq.com>
2018-11-16 14:43:24 +0800

    添加代码高亮

--- src/components/row.vue
+++ src/components/row.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="row">
     Row
+    <pre v-highlightjs="sourcecode"><code class="html"></code></pre>
+    <pre v-highlightjs><code class="html">const s = new Date().toString()</code></pre>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Row'
+    name: 'Row',
+    data() {
+        return {
+            sourcecode: '<p>heh</p>'
+        };
+    }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
+import VueHighlightJS from 'vue-highlightjs';
+import 'highlight.js/styles/atelier-dune-light.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
-
+Vue.use(VueHighlightJS);
 new Vue({
     router,
     store,
